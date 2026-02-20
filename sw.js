04ea99ae5c32143892180fd44a72fca85eade521
2hanjong-design <2hanjong@gmail.com>
2026-02-20 12:58:04 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,5 @@
-const CACHE_NAME = "drum-app-netlify-v6";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./icon-192-v2.png",
-  "./icon-512-v2.png"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((res) => res || fetch(event.request))
-  );
-});
+const CACHE_NAME = "drum-calc-v4";
+const ASSETS = ["./","./index.html","./manifest.webmanifest","./sw.js","./icons/icon-192.png","./icons/icon-512.png"];
+self.addEventListener("install",(e)=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener("activate",(e)=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE_NAME?null:caches.delete(k)))));self.clients.claim();});
+self.addEventListener("fetch",(e)=>{e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request)));});
